d8a75678ba625a1a9c76be6168aa9d6401139ab8
ethan <ethan@maru.jp>
2019-11-25 17:03:59 +0900

    Initial Commit

---
+++ mediasoup-ios-cient-sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mediasoup-ios-cient-sample
+//
+//  Created by Denvir Ethan on 2019/11/25.
+//  Copyright © 2019 Denvir Ethan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
